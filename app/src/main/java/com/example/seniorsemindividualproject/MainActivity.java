7ca2f185e7dfa3e82cf415a1ad73b34e04df2eed
Walker Hayes <112342932+wthayes@users.noreply.github.com>
2023-02-13 20:21:58 -0500

    Project is finished. It takes a photo from the files and assigns it a random classification.

--- app/src/main/java/com/example/seniorsemindividualproject/MainActivity.java
+++ app/src/main/java/com/example/seniorsemindividualproject/MainActivity.java
@@ -2,17 +2,28 @@ package com.example.seniorsemindividualproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.io.DataInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.Random;
 import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView identifyTxt;
+    ImageView loadedImg;
+    Random rand;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,27 +31,50 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         identifyTxt = (TextView) findViewById(R.id.identifyTxt);
+        loadedImg = (ImageView) findViewById(R.id.loadedImg);
+        rand = new Random();
     }
 
-    public String randomString(){
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1 && resultCode == RESULT_OK && data != null) {
+            try {
+                InputStream IS = getContentResolver().openInputStream(data.getData());
+                Bitmap BM = BitmapFactory.decodeStream(IS);
+                loadedImg.setImageBitmap(BM);
+                String s = "That is ";
+                s += randomString();
+                s += ".";
+                identifyTxt.setText(s);
+            } catch (Exception e) {
+                identifyTxt.setText(e.toString());
+            }
+        }
+    }
+
+    public void loadImage(View view) {
+        Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+        i.setType("image/*");
+
+        startActivityForResult(Intent.createChooser(i, "Pick and Image"), 1);
+    }
+
+    public String randomString() {
         String s = "";
-        Random rand = new Random();
-        try{
+        try {
             DataInputStream stringFile = new DataInputStream(getAssets().open("PictureStrings.txt"));
             Scanner scan = new Scanner(stringFile);
-            for(int i = 0; i < rand.nextInt(50); i++){
+            int num = (int) (rand.nextDouble() * 50);
+            for (int i = 0; i < num; i++) {
                 scan.nextLine();
             }
             s = scan.nextLine();
             scan.close();
-        }catch (Exception e){
+        } catch (Exception e) {
             return s;
         }
         return s;
-    }
-
-    public void btnPress(View view){
-        String s = randomString();
-        identifyTxt.setText(s);
     }
 }
